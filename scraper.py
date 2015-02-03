a3f5205b8d9e761b7fbc57d7a96c24b0fd181505
Srivats P <pstavirs@users.noreply.github.com>
2015-02-03 23:46:24 +0530

    Updated scraper to not just update database with last day's count(s), but all days since the last record in the db till the previous day

--- scraper.py
+++ scraper.py
@@ -3,8 +3,14 @@ import lxml.html
 import re
 import requests
 import scraperwiki
+from datetime import date as Date
 
 downloads = {}
+
+#scraperwiki.sqlite.execute("delete from downloads where Date > '2015-01-18'")
+
+last_date = scraperwiki.sqlite.select("max(Date) from downloads where Date < '" + Date.today().isoformat() + "'")[0]['max(Date)']
+print "Last date in DB: "+last_date
 
 for package in ['ostinato-bin-win32', 'ostinato-bin-osx-universal', 'ostinato-src']:
     page = requests.get('https://bintray.com/package/statistics/pstavirs/ostinato/'+package) 
@@ -29,15 +35,42 @@ for package in ['ostinato-bin-win32', 'ostinato-bin-osx-universal', 'ostinato-sr
     # json schema -
     # [{name : <version>, data : [[<date>, <count>], ...]}]
     # data ordered from older to newer dates
+    version = j[0]['name']
+    data = j[0]['data']
 
-    # since today is not yet over, get yesterday's count 
-    name = package+'-'+j[0]['name']
-    date = j[0]['data'][-2][0]
-    count = j[0]['data'][-2][1]
-    #print date, name, count
-    downloads['Date'] = date
-    downloads[name] = count
+    # since today is not yet over, remove today's count
+    data.pop()
 
-print downloads
-scraperwiki.sqlite.save(unique_keys=['Date'], data=downloads, table_name='downloads')
+    #print version
+    #print data
+
+    name = package+'-'+version
+    for tuple in data:
+        # tuple = [<date>, <count>]
+        # skip tuples that we've already stored in the db
+        if (tuple[0] <= last_date):
+            continue
+        date = tuple[0]
+        count = tuple[1]
+        #print date, name, count
+        if (not downloads.has_key(date)):
+            downloads[date] = {}
+        downloads[date][name] = count
+
+# downloads is a dict with schema -
+#       { date: {name: count, name: count, ...}, ...}
+# we need to convert it into a list of dicts with schema -
+#       [ {Date: date, name: count, name: count, ...}, ...]
+#print downloads
+keys = downloads.keys()
+# sort in increasing order of dates
+keys.sort()
+records = []
+for d in keys:
+    x = downloads[d]
+    x['Date'] = d
+    records.append(x)
+print records
+if len(records):
+    scraperwiki.sqlite.save(unique_keys=['Date'], data=records, table_name='downloads')
 
