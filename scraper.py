29cf3d2cc8bf3e6340e8c735d4f9cc238b566be4
Srivats P <pstavirs@users.noreply.github.com>
2015-04-12 12:05:14 +0530

    Updated scraper code to work with recent changes by Bintray of their stats page which broke the older scraper

--- scraper.py
+++ scraper.py
@@ -1,61 +1,49 @@
 import json
-import lxml.html
-import re
 import requests
 import scraperwiki
-from datetime import date as Date
+from datetime import date as Date, datetime
 
 downloads = {}
 
-#scraperwiki.sqlite.execute("delete from downloads where Date > '2015-01-18'")
+#scraperwiki.sqlite.execute("delete from downloads where Date > '2015-03-29'")
 
 last_date = scraperwiki.sqlite.select("max(Date) from downloads where Date < '" + Date.today().isoformat() + "'")[0]['max(Date)']
 print "Last date in DB: "+last_date
 
 for package in ['ostinato-bin-win32', 'ostinato-bin-osx-universal', 'ostinato-src']:
-    page = requests.get('https://bintray.com/package/statistics/pstavirs/ostinato/'+package) 
-    doc = lxml.html.document_fromstring(page.text)
-
-    # get the javascript snippet containing the downloads data
-    script = doc.xpath("//div[@id='show-pkg']/script[2]")[0]
-    #print script.text
-
-    # extract just the downloads data from the javascript
-    m = re.search('series:(.*),\s+legend', script.text, re.DOTALL)
-
-    # massage the javascript object to make it valid json
-    n = re.sub('Date.UTC\((\d+), (\d+)-1, (\d+)\)', '"\g<1>-\g<2>-\g<3>"', m.group(1))
-    n = re.sub('name|data', '"\g<0>"', n);
-    #print n
+    # retrieve the statistics json data
+    page = requests.get('https://bintray.com/statistics/packageStatistics?pkgPath=/pstavirs/ostinato/'+package)
+    #print page.text
 
     # parse the json
-    j = json.loads(n)
-    #print json.dumps(j[0])
+    j = json.loads(page.text)
+    #print json.dumps(j)
 
     # json schema -
-    # [{name : <version>, data : [[<date>, <count>], ...]}]
-    # data ordered from older to newer dates
-    version = j[0]['name']
-    data = j[0]['data']
+    # { ..., data:[{version:<version>, series:[[<timestamp>, <count>], ...]}]
+    # series timestamps ordered from older to newer dates; timestamps in msec
+    for version_data in j['data']:
+        version = version_data['version']
+        data = version_data['series']
 
-    # since today is not yet over, remove today's count
-    data.pop()
+        # since today is not yet over, remove today's count
+        data.pop()
 
-    #print version
-    #print data
+        #print version
+        #print data
 
-    name = package+'-'+version
-    for tuple in data:
-        # tuple = [<date>, <count>]
-        # skip tuples that we've already stored in the db
-        if (tuple[0] <= last_date):
-            continue
-        date = tuple[0]
-        count = tuple[1]
-        #print date, name, count
-        if (not downloads.has_key(date)):
-            downloads[date] = {}
-        downloads[date][name] = count
+        name = package+'-'+version
+        for tuple in data:
+            # tuple = [<timestamp>, <count>]
+            # skip tuples that we've already stored in the db
+            date = datetime.fromtimestamp(float(tuple[0])/1e3).__str__()[:10]
+            count = tuple[1]
+            if (date <= last_date):
+                continue
+            print date, name, count
+            if (not downloads.has_key(date)):
+                downloads[date] = {}
+            downloads[date][name] = count
 
 # downloads is a dict with schema -
 #       { date: {name: count, name: count, ...}, ...}
